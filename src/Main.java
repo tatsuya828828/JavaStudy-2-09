01f28ecd76568b3ef5d7d57cd581b7e6ccdeee20
tatsuya828828 <kkjy011894@gmail.com>
2020-07-15 21:55:23 +0900

    [finish]09-07

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,16 @@
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 	public static void main(String[] args) {
-		String msg = "第一土曜日";
-		// StringReaderクラスは、指定したString型変数から1文字ずつ読み取るための機能を提供する
-		// このクラスはFileReaderクラスとにているが、終端がファイルではなくコンストラクタで指定された文字列である
-		try(Reader sr = new StringReader(msg)){
-			if(msg instanceof String) {
-				char c1 = (char) sr.read();
-				char c2 = (char) sr.read();
-				char c3 = (char) sr.read();
-				char c4 = (char) sr.read();
-				char c5 = (char) sr.read();
-				System.out.print(c1);
-				System.out.print(c2);
-				System.out.print(c3);
-				System.out.print(c4);
-				System.out.print(c5);
-			}
-		} catch(IOException e) {
-			System.out.println("エラーです");
-			e.printStackTrace();
-		}
+		// Pathインターフェースのgetメソッドを使うと特定のファイルを指し示すことができる
+		// もしくは既にFileインスタンスが存在する場合は、toPathメソッドを使うことによってPathインスタンスに変換できる
+		// Pathインスタンスは、あくまでファイルやフォルダを指し示す役割しか持たない
+		// 実際に、指し示したものを操作するためのメソッドは、Filesクラスに定義されている
+		Path p = Paths.get("rpgsave.dat");
+		// Filesクラスのexistsメソッドを使うと、ファイルが存在するか確認してくれる
+		boolean f = Files.exists(p);
+		System.out.println(f);
 	}
 }

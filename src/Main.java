e936bea0e408bcd74de92ea3aaa96ebec473bbfe
tatsuya828828 <kkjy011894@gmail.com>
2020-07-15 21:33:16 +0900

    [finish]09-05

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 
 public class Main {
 	public static void main(String[] args) {
-		// try-catch構文を使って、例外処理を定義しておく
-		try(FileWriter fw = new FileWriter("rpgsave.dat", true)){
-			fw.write('A');
-			fw.flush();
-		} catch(IOException e) {
-			System.out.println("ファイル書き込みエラーです");
-			e.printStackTrace();
-		}
-
-		try(FileReader fr = new FileReader("rpgsave.dat")){
-			int i = fr.read();
-			while(i != -1) {
-				char c = (char) i;
-				System.out.print(c);
-				i = fr.read();
+		String msg = "第一土曜日";
+		// StringReaderクラスは、指定したString型変数から1文字ずつ読み取るための機能を提供する
+		// このクラスはFileReaderクラスとにているが、終端がファイルではなくコンストラクタで指定された文字列である
+		try(Reader sr = new StringReader(msg)){
+			if(msg instanceof String) {
+				char c1 = (char) sr.read();
+				char c2 = (char) sr.read();
+				char c3 = (char) sr.read();
+				char c4 = (char) sr.read();
+				char c5 = (char) sr.read();
+				System.out.print(c1);
+				System.out.print(c2);
+				System.out.print(c3);
+				System.out.print(c4);
+				System.out.print(c5);
 			}
 		} catch(IOException e) {
-			System.out.println("ファイル読み込みエラーです");
+			System.out.println("エラーです");
 			e.printStackTrace();
 		}
 	}
